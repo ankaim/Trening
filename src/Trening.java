96d8e3206dfd5b1b4bfc2e5b8cbdeccaf4b4778f
ankaim <kaim@mail.ru>
2017-06-16 11:06:54 +0200

    Создан класс Дом и протестирован!

--- src/Trening.java
+++ src/Trening.java
@@ -1,6 +1,23 @@
 
 public class Trening {
     public static void main(String[] args) {
-
+        Hause hause = new Hause("Hause", 20);
+        hause.view();
     }
+    static class Hause{
+        String name;
+        int width;
+
+        public Hause() {
+        }
+
+        public Hause(String name, int width) {
+            this.name = name;
+            this.width = width;
+        }
+        public void view(){
+            System.out.println("Name = "+name+"; Width = "+width);
+        }
+    }
+    
 }
