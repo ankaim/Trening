afd28636e478c5e82c1f4f0c9c39c03c286fc3c1
ankaim <kaim@mail.ru>
2017-06-16 13:53:28 +0200

    Создан класс наследник  и протестирован.

--- src/Trening.java
+++ src/Trening.java
@@ -2,7 +2,11 @@
 public class Trening {
     public static void main(String[] args) {
         Hause hause = new Hause("Hause", 20);
-        hause.view();
+        hause.viewHaus();
+        Door door = new Door("Дверь в доме", 40, 50);
+        door.viewDoor();
+        door.viewHaus();
+
     }
     static class Hause{
         String name;
@@ -15,9 +19,20 @@ public class Trening {
             this.name = name;
             this.width = width;
         }
-        public void view(){
+        public void viewHaus(){
             System.out.println("Name = "+name+"; Width = "+width);
         }
     }
-    
+    static class Door extends Hause{
+        int height;
+
+        public Door(String name, int width, int height) {
+            super(name, width);
+            this.height = height;
+        }
+
+        public void viewDoor(){
+            System.out.println("Name = "+name+"; Width = "+width+ "; Height = "+height);
+        }
+    }
 }
